0a2a3944200a4da1be2b2b53b512a8162ad6b5d5
xiong <wenhua.xiong@biitt.cn>
2019-04-04 11:24:16 +0800

    .

--- order-service/src/main/java/com/example/demo/controller/OrderController.java
+++ order-service/src/main/java/com/example/demo/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +12,10 @@ public class OrderController {
 
     @Autowired
     OrderService orderService;
-    @Value("${test.aaa}")
-    String aaa;
 
     @GetMapping("list")
     public String list(){
-//        zreturn orderService.list();
-        return aaa;
+        return orderService.list();
     }
 
 }
